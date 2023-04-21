ce9eb215ecb49312b5452aa462c14db53c996f09
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-04-21 20:37:19 +0700

    delete server

---
+++ server/server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	server := http.Server{
+		Addr: "localhost:8080",
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
