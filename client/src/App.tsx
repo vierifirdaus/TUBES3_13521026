caebd87e303c29f4ef2a433739fc9e912296130a
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-04-23 01:57:59 +0700

    Fix : Home.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 
 
 function App() {
--- client/src/components/chat.tsx
+++ client/src/components/chat.tsx
@@ -12,77 +12,77 @@ interface message {
 }
 
 const messages: message[]= [
-    {
-        sender: 'user',
-        text: 'Hello'
-    },
-    {
-        sender: 'bot',
-        text: 'Hi'
-    },
-    {
-        sender: 'user',
-        text: 'How are you?'
-    },
-    {
-        sender: 'bot',
-        text: 'I am fine'
-    },
-    {
-        sender: 'user',
-        text: 'What are you doing?'
-    },
-    {
-        sender: 'bot',
-        text: 'I am chatting with you'
-    },
-    {
-        sender: 'user',
-        text: 'What is your name?'
-    },
-    {
-        sender: 'bot',
-        text: 'My name is Chatbot'
-    },
-    {
-        sender: 'user',
-        text: 'What is your age?'
-    },
-    {
-        sender: 'bot',
-        text: 'I am 1 year old'
-    },
-    {
-        sender: 'user',
-        text:'What is your favourite food?'
-    },
-    {
-        sender: 'bot',
-        text: 'I like to eat data'
-    },
-    {
-        sender: 'user',
-        text: 'What is your favourite color?'
-    },
-    {
-        sender: 'bot',
-        text: 'I like to be black'
-    },
-    {
-        sender: 'user',
-        text: 'What is your favourite movie?'
-    },
-    {
-        sender: 'bot',
-        text: 'I like to watch The Matrix'
-    }
+    // {
+    //     sender: 'user',
+    //     text: 'Hello'
+    // },
+    // {
+    //     sender: 'bot',
+    //     text: 'Hi'
+    // },
+    // {
+    //     sender: 'user',
+    //     text: 'How are you?'
+    // },
+    // {
+    //     sender: 'bot',
+    //     text: 'I am fine'
+    // },
+    // {
+    //     sender: 'user',
+    //     text: 'What are you doing?'
+    // },
+    // {
+    //     sender: 'bot',
+    //     text: 'I am chatting with you'
+    // },
+    // {
+    //     sender: 'user',
+    //     text: 'What is your name?'
+    // },
+    // {
+    //     sender: 'bot',
+    //     text: 'My name is Chatbot'
+    // },
+    // {
+    //     sender: 'user',
+    //     text: 'What is your age?'
+    // },
+    // {
+    //     sender: 'bot',
+    //     text: 'I am 1 year old'
+    // },
+    // {
+    //     sender: 'user',
+    //     text:'What is your favourite food?'
+    // },
+    // {
+    //     sender: 'bot',
+    //     text: 'I like to eat data'
+    // },
+    // {
+    //     sender: 'user',
+    //     text: 'What is your favourite color?'
+    // },
+    // {
+    //     sender: 'bot',
+    //     text: 'I like to be black'
+    // },
+    // {
+    //     sender: 'user',
+    //     text: 'What is your favourite movie?'
+    // },
+    // {
+    //     sender: 'bot',
+    //     text: 'I like to watch The Matrix'
+    // }
 ]
 
 const Chat : React.FC<chatProps> = ({className}) => {
   return (
     <div className={className}>
         <div className='min-h-full'>
-        { messages.length !== 0 && messages.map((message) => (
+        { messages.map((message) => (
             <Message message={message}/>
         ))}
         </div>
